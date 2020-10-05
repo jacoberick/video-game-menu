8236737dda16584e4d01cb16030c26e10cc04800
jacoberick <jacobewhitedev@gmail.com>
2020-10-04 23:57:00 -0500

    Updates

---
+++ precache-manifest.8b5ebc27266a77487b82ff5e87cb03d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85223155f8bde9029f36c7ed72b9058d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c447a216f47835b96ea",
+    "url": "/static/css/main.b0a0667d.chunk.css"
+  },
+  {
+    "revision": "62e56e40e202db8c9202",
+    "url": "/static/js/2.75af5f42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75af5f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c447a216f47835b96ea",
+    "url": "/static/js/main.b979fa97.chunk.js"
+  },
+  {
+    "revision": "4649c6026b20013209a2",
+    "url": "/static/js/runtime-main.1d676a9d.js"
+  },
+  {
+    "revision": "0e9e4c883816ae5b12e13943fae6b61d",
+    "url": "/static/media/background.0e9e4c88.jpg"
+  },
+  {
+    "revision": "bc31dc9a9be9c8306c1183f75cfb39cd",
+    "url": "/static/media/campaign.bc31dc9a.mp4"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43924d9fa046e187bdf772f6805f07c5",
+    "url": "/static/media/multiplayer.43924d9f.mp4"
+  },
+  {
+    "revision": "f46e9841755405005fa5fd295ea98627",
+    "url": "/static/media/specops.f46e9841.mp4"
+  }
+]);
